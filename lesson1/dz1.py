de0c66ac2d43386f5478286705113e5f19c63116
rustam <spycer@yandex.ru>
2021-08-14 20:21:36 +0300

    Привет

--- lesson1/dz1.py
+++ lesson1/dz1.py
@@ -1,1 +1,18 @@
-print('hello word')
+
+import requests
+import json
+
+url = 'https://api.github.com/users/'
+username = 'Spyker086'
+p = url + username + '/repos'
+
+repos = requests.get(url + username + '/repos').json()
+repos_list = []
+
+for i in range(len(repos)):
+    repos_list.append(repos[i].get('name'))
+
+with open('task1.json', 'w') as f:
+    json.dump(repos_list, f)
+
+print(repos_list)
